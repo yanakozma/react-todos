a1660d068bd2cbcec208ca283b84670d4c2105f1
Yana Kozma <jane.kozma@gmail.com>
2024-07-30 06:20:01 -0400

    add all files

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import CssBaseline from "@mui/material/CssBaseline"
+import './App.css'
+import TodoList from "./TodoList"
+import Navbar from "./Navbar";
+
+function App() {
+  return (
+    <>
+      <CssBaseline />
+      <Navbar/>
+      <TodoList/>
+    </>
+  )
+}
+
+export default App
